0c371b15798154ac472b3fda6f8556ccfa44166d
mizan21 <mizanorrahman398@gmail.com>
2024-11-25 12:02:36 +0600

    Documents, Payments Info, HRDetails part added

--- app/ui/Employee/AddEmployee/Documents.jsx
+++ app/ui/Employee/AddEmployee/Documents.jsx
@@ -2,32 +2,288 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
+const InputField = ({
+  label,
+  type,
+  register,
+  name,
+  validation,
+  placeholder,
+  errors,
+}) => (
+  <div>
+    <label className="block text-sm font-medium text-gray-600">{label}</label>
+    <input
+      type={type}
+      {...register(name, validation)}
+      className={`border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full ${
+        errors?.[name] ? "border-red-500" : "border-gray-300"
+      }`}
+      placeholder={placeholder}
+    />
+    {errors?.[name] && (
+      <p className="text-red-500 text-sm mt-1">{errors[name].message}</p>
+    )}
+  </div>
+);
+
+const FileUploadField = ({ label, register, name, validation, errors }) => (
+  <div>
+    <label className="block text-sm font-medium text-gray-600">{label}</label>
+    <input
+      type="file"
+      {...register(name, validation)}
+      className={`border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full ${
+        errors?.[name] ? "border-red-500" : "border-gray-300"
+      }`}
+    />
+    {errors?.[name] && (
+      <p className="text-red-500 text-sm mt-1">{errors[name].message}</p>
+    )}
+  </div>
+);
+
+const SelectField = ({
+  label,
+  register,
+  name,
+  options,
+  validation,
+  errors,
+}) => (
+  <div>
+    <label className="block text-sm font-medium text-gray-600">{label}</label>
+    <select
+      {...register(name, validation)}
+      className={`border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full ${
+        errors?.[name] ? "border-red-500" : "border-gray-300"
+      }`}
+    >
+      <option value="">Select {label}</option>
+      {options.map((option, idx) => (
+        <option key={idx} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+    {errors?.[name] && (
+      <p className="text-red-500 text-sm mt-1">{errors[name].message}</p>
+    )}
+  </div>
+);
+
 const Documents = () => {
-  const { register, handleSubmit, reset } = useForm();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = (data) => {
-    console.log("Documents:", data);
-    reset(); // Reset form after submission
+    console.log("Document Form Submitted:", data);
+    reset();
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-      <h3 className="text-2xl font-bold text-gray-800">Documents</h3>
-      <div>
-        <label className="block text-sm font-medium text-gray-600">
-          Upload Resume
-        </label>
-        <input
-          type="file"
-          {...register("resume", { required: "Resume is required" })}
-          className="border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full"
-        />
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
+      <h3 className="text-2xl font-bold text-gray-800">
+        Add Document Information
+      </h3>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+        {/* Hired From */}
+        <div className="border-2 border-dashed p-5 rounded-md space-y-6">
+          <h4 className="text-xl font-semibold text-gray-700 mb-4">
+            Hired From
+          </h4>
+          <InputField
+            label="Hired From"
+            type="text"
+            register={register}
+            name="hiredFrom"
+            validation={{ required: "Hired From is required" }}
+            placeholder="Enter full name you hired from"
+            errors={errors}
+          />
+          <InputField
+            label="Contact Full Name"
+            type="text"
+            register={register}
+            name="hiredFromContactName"
+            validation={{ required: "Contact Full Name is required" }}
+            placeholder="Enter full name"
+            errors={errors}
+          />
+          <InputField
+            label="Contact Number"
+            type="text"
+            register={register}
+            name="hiredFromContactNumber"
+            validation={{ required: "Contact Number is required" }}
+            placeholder="Enter contact number"
+            errors={errors}
+          />
+          <InputField
+            label="Date"
+            type="date"
+            register={register}
+            name="hiredFromDate"
+            validation={{ required: "Date is required" }}
+            errors={errors}
+          />
+          <InputField
+            label="Expiry Date"
+            type="date"
+            register={register}
+            name="hiredFromExpiryDate"
+            validation={{ required: "Expiry Date is required" }}
+            errors={errors}
+          />
+          <FileUploadField
+            label="Documents"
+            register={register}
+            name="hiredFromDocuments"
+            validation={{ required: "Documents are required" }}
+            errors={errors}
+          />
+        </div>
+
+        {/* Hired By */}
+        <div className="border-2 border-dashed p-5 rounded-md space-y-6">
+          <h4 className="text-xl font-semibold text-gray-700 mb-4">Hired By</h4>
+          <InputField
+            label="Hired By"
+            type="text"
+            register={register}
+            name="hiredBy"
+            validation={{ required: "Hired By is required" }}
+            placeholder="Enter full name you hired by"
+            errors={errors}
+          />
+          <InputField
+            label="Contact Full Name"
+            type="text"
+            register={register}
+            name="hiredByContactName"
+            validation={{ required: "Contact Full Name is required" }}
+            placeholder="Enter full name"
+            errors={errors}
+          />
+          <InputField
+            label="Contact Number"
+            type="text"
+            register={register}
+            name="hiredByContactNumber"
+            validation={{ required: "Contact Number is required" }}
+            placeholder="Enter contact number"
+            errors={errors}
+          />
+          <InputField
+            label="Date"
+            type="date"
+            register={register}
+            name="hiredByDate"
+            validation={{ required: "Date is required" }}
+            errors={errors}
+          />
+          <InputField
+            label="Expiry Date"
+            type="date"
+            register={register}
+            name="hiredByExpiryDate"
+            validation={{ required: "Expiry Date is required" }}
+            errors={errors}
+          />
+          <FileUploadField
+            label="Documents"
+            register={register}
+            name="hiredByDocuments"
+            validation={{ required: "Documents are required" }}
+            errors={errors}
+          />
+          <InputField
+            label="NOC Expiry Date"
+            type="date"
+            register={register}
+            name="nocExpiryDate"
+            validation={{ required: "NOC Expiry Date is required" }}
+            errors={errors}
+          />
+        </div>
+
+        {/* VISA Information */}
+        <div className="border-2 border-dashed p-5 rounded-md space-y-6">
+          <h4 className="text-xl font-semibold text-gray-700 mb-4">
+            VISA Information
+          </h4>
+          <FileUploadField
+            label="VISA Proof"
+            register={register}
+            name="visaProof"
+            validation={{ required: "VISA Proof is required" }}
+            errors={errors}
+          />
+          <SelectField
+            label="VISA Type"
+            register={register}
+            name="visaType"
+            options={[
+              { label: "Work Visa", value: "work" },
+              { label: "Tourist Visa", value: "tourist" },
+              { label: "Business Visa", value: "business" },
+            ]}
+            validation={{ required: "VISA Type is required" }}
+            errors={errors}
+          />
+          <InputField
+            label="Entry Date"
+            type="date"
+            register={register}
+            name="visaEntryDate"
+            validation={{ required: "Entry Date is required" }}
+            errors={errors}
+          />
+          <InputField
+            label="VISA Expiry Date"
+            type="date"
+            register={register}
+            name="visaExpiryDate"
+            validation={{ required: "VISA Expiry Date is required" }}
+            errors={errors}
+          />
+          <InputField
+            label="Medical Date"
+            type="date"
+            register={register}
+            name="visaMedicalDate"
+            validation={{ required: "Medical Date is required" }}
+            errors={errors}
+          />
+          <InputField
+            label="Fingerprint Date"
+            type="date"
+            register={register}
+            name="visaFingerprintDate"
+            validation={{ required: "Fingerprint Date is required" }}
+            errors={errors}
+          />
+          <InputField
+            label="Deadline"
+            type="date"
+            register={register}
+            name="visaDeadline"
+            validation={{ required: "Deadline is required" }}
+            errors={errors}
+          />
+        </div>
       </div>
+
       <button
         type="submit"
         className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
       >
-        Save Documents
+        Save Document Information
       </button>
     </form>
   );
--- app/ui/Employee/AddEmployee/PaymentInformation.jsx
+++ app/ui/Employee/AddEmployee/PaymentInformation.jsx
@@ -1,49 +1,363 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const PaymentInformation = () => {
-  const { register, handleSubmit, reset } = useForm();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      basicPay: 0,
+      overtimeHours: 0,
+      holidayHours: 0,
+      commission: 0,
+      accAllowance: 0,
+      foodAllowance: 0,
+      telephoneAllowance: 0,
+      transportAllowance: 0,
+    },
+  });
+
+  const [showExtras, setShowExtras] = useState(false);
+
+  // Watch values for dynamic calculations
+  const watchFields = watch();
+  const basicPay = parseFloat(watchFields.basicPay) || 0;
+  const overtimeHours = parseFloat(watchFields.overtimeHours) || 0;
+  const holidayHours = parseFloat(watchFields.holidayHours) || 0;
+  const commissionPercentage = parseFloat(watchFields.commission) || 0;
+  const allowances = {
+    accAllowance: parseFloat(watchFields.accAllowance) || 0,
+    foodAllowance: parseFloat(watchFields.foodAllowance) || 0,
+    telephoneAllowance: parseFloat(watchFields.telephoneAllowance) || 0,
+    transportAllowance: parseFloat(watchFields.transportAllowance) || 0,
+  };
+
+  // Calculate Total Pay
+  const calculateTotalPay = () => {
+    const overtimePay = (basicPay / 208) * 1.25 * overtimeHours;
+    const holidayPay = (basicPay / 208) * 1.5 * holidayHours;
+    const commissionPay = (basicPay * commissionPercentage) / 100;
+    const totalAllowances = Object.values(allowances).reduce(
+      (sum, value) => sum + value,
+      0
+    );
+    return (
+      basicPay + overtimePay + holidayPay + commissionPay + totalAllowances
+    );
+  };
 
   const onSubmit = (data) => {
-    console.log("Payment Information:", data);
-    reset(); // Reset form after submission
+    const totalPay = calculateTotalPay();
+    console.log("Form Submitted:", { ...data, totalPay });
+    alert(`Total Pay: ${totalPay.toFixed(2)}`);
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-      <h3 className="text-2xl font-bold text-gray-800">Payment Information</h3>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-        <div>
-          <label className="block text-sm font-medium text-gray-600">
-            Bank Name
-          </label>
-          <input
-            type="text"
-            {...register("bankName", { required: "Bank name is required" })}
-            className="border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full"
-            placeholder="Enter Bank Name"
-          />
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
+      <h3 className="text-2xl font-bold text-gray-800">
+        Add Employee Work Information
+      </h3>
+
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+        {/* Employee Work Information */}
+        <div className="col-span-1 border-2 border-dashed p-5 rounded-md">
+          <h4 className="text-xl font-semibold text-gray-700 mb-4">
+            Employee Work Information
+          </h4>
+          <div className="grid grid-cols-1 gap-6">
+            {/* Employee Type */}
+            <div>
+              <label className="block text-sm font-medium text-gray-600">
+                Employee Type *
+              </label>
+              <div className="flex space-x-2">
+                {["Hourly", "Daily", "Monthly", "Others"].map((type) => (
+                  <label key={type} className="flex items-center space-x-2">
+                    <input
+                      type="radio"
+                      value={type}
+                      {...register("employeeType", {
+                        required: "Employee Type is required",
+                      })}
+                      className="mr-1"
+                    />
+                    <span>{type}</span>
+                  </label>
+                ))}
+              </div>
+              {errors.employeeType && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.employeeType.message}
+                </p>
+              )}
+            </div>
+
+            {/* Shifts */}
+            <div>
+              <label className="block text-sm font-medium text-gray-600">
+                First Shift *
+              </label>
+              <div className="flex space-x-2">
+                <input
+                  type="time"
+                  {...register("firstShiftStart", {
+                    required: "Start time is required",
+                  })}
+                  className="border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-1/2"
+                />
+                <input
+                  type="time"
+                  {...register("firstShiftEnd", {
+                    required: "End time is required",
+                  })}
+                  className="border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-1/2"
+                />
+              </div>
+            </div>
+
+            {/* Basic Pay */}
+            <div>
+              <label className="block text-sm font-medium text-gray-600">
+                Basic Pay *
+              </label>
+              <input
+                type="number"
+                {...register("basicPay", { required: "Basic Pay is required" })}
+                className="border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full"
+                placeholder="Enter basic pay"
+              />
+              {errors.basicPay && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.basicPay.message}
+                </p>
+              )}
+            </div>
+
+            {/* Extra Fields Toggle */}
+            <div className="mt-4">
+              <label className="flex items-center space-x-2">
+                <input
+                  type="checkbox"
+                  onChange={() => setShowExtras(!showExtras)}
+                  className="mr-2"
+                />
+                <span className="font-medium text-gray-600">
+                  Extra (Optional)
+                </span>
+              </label>
+            </div>
+
+            {showExtras && (
+              <div className="grid grid-cols-2 gap-4 mt-4">
+                {/* Over Time */}
+                <div>
+                  <label className="block text-sm font-medium text-gray-600">
+                    Over Time Hours (Normal @1.25)
+                  </label>
+                  <input
+                    type="number"
+                    {...register("overtimeHours")}
+                    className="border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full"
+                    placeholder="Enter hours"
+                  />
+                </div>
+
+                {/* Holiday OT */}
+                <div>
+                  <label className="block text-sm font-medium text-gray-600">
+                    Holiday OT (@1.50)
+                  </label>
+                  <input
+                    type="number"
+                    {...register("holidayHours")}
+                    className="border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full"
+                    placeholder="Enter hours"
+                  />
+                </div>
+
+                {/* Commission */}
+                <div>
+                  <label className="block text-sm font-medium text-gray-600">
+                    Commission (%)
+                  </label>
+                  <input
+                    type="number"
+                    {...register("commission")}
+                    className="border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full"
+                    placeholder="Enter percentage"
+                  />
+                </div>
+
+                {/* Allowances */}
+                <div>
+                  <label className="block text-sm font-medium text-gray-600">
+                    ACC Allowance
+                  </label>
+                  <input
+                    type="number"
+                    {...register("accAllowance")}
+                    className="border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full"
+                    placeholder="Enter amount"
+                  />
+                </div>
+                <div>
+                  <label className="block text-sm font-medium text-gray-600">
+                    Food Allowance
+                  </label>
+                  <input
+                    type="number"
+                    {...register("foodAllowance")}
+                    className="border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full"
+                    placeholder="Enter amount"
+                  />
+                </div>
+                <div>
+                  <label className="block text-sm font-medium text-gray-600">
+                    Telephone Allowance
+                  </label>
+                  <input
+                    type="number"
+                    {...register("telephoneAllowance")}
+                    className="border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full"
+                    placeholder="Enter amount"
+                  />
+                </div>
+                <div>
+                  <label className="block text-sm font-medium text-gray-600">
+                    Transport Allowance
+                  </label>
+                  <input
+                    type="number"
+                    {...register("transportAllowance")}
+                    className="border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full"
+                    placeholder="Enter amount"
+                  />
+                </div>
+              </div>
+            )}
+          </div>
         </div>
-        <div>
-          <label className="block text-sm font-medium text-gray-600">
-            Account Number
-          </label>
-          <input
-            type="text"
-            {...register("accountNumber", {
-              required: "Account number is required",
-            })}
-            className="border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full"
-            placeholder="Enter Account Number"
-          />
+
+        <div className="col-span-2 space-y-5">
+          {/* Vendor Billing Information */}
+          <div className="border-2 border-dashed p-5 rounded-md">
+            <h4 className="text-xl font-semibold text-gray-700 mb-4">
+              Vendor Billing Information *
+            </h4>
+            <div className="space-y-4">
+              <div>
+                <label className="block text-sm font-medium text-gray-600">
+                  Vendor Name *
+                </label>
+                <input
+                  type="text"
+                  {...register("vendorName", {
+                    required: "Vendor Name is required",
+                  })}
+                  className="border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full"
+                  placeholder="Enter vendor name"
+                />
+              </div>
+              <div className="grid grid-cols-2 gap-4">
+                <div>
+                  <label className="block text-sm font-medium text-gray-600">
+                    Working Hours *
+                  </label>
+                  <input
+                    type="number"
+                    {...register("vendorWorkingHours", {
+                      required: "Working Hours are required",
+                    })}
+                    className="border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full"
+                    placeholder="Enter hours"
+                  />
+                </div>
+                <div>
+                  <label className="block text-sm font-medium text-gray-600">
+                    Rate *
+                  </label>
+                  <input
+                    type="number"
+                    {...register("vendorRate", {
+                      required: "Rate is required",
+                    })}
+                    className="border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full"
+                    placeholder="Enter rate"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Customer Billing Information */}
+          <div className="border-2 border-dashed p-5 rounded-md">
+            <h4 className="text-xl font-semibold text-gray-700 mb-4">
+              Customer Billing Information *
+            </h4>
+            <div className="space-y-4">
+              <div>
+                <label className="block text-sm font-medium text-gray-600">
+                  Customer Name *
+                </label>
+                <input
+                  type="text"
+                  {...register("customerName", {
+                    required: "Customer Name is required",
+                  })}
+                  className="border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full"
+                  placeholder="Enter customer name"
+                />
+              </div>
+              <div className="grid grid-cols-2 gap-4">
+                <div>
+                  <label className="block text-sm font-medium text-gray-600">
+                    Working Hours *
+                  </label>
+                  <input
+                    type="number"
+                    {...register("customerWorkingHours", {
+                      required: "Working Hours are required",
+                    })}
+                    className="border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full"
+                    placeholder="Enter hours"
+                  />
+                </div>
+                <div>
+                  <label className="block text-sm font-medium text-gray-600">
+                    Rate *
+                  </label>
+                  <input
+                    type="number"
+                    {...register("customerRate", {
+                      required: "Rate is required",
+                    })}
+                    className="border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200 w-full"
+                    placeholder="Enter rate"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+
+      {/* Total Pay */}
+      <div className="mt-6">
+        <h4 className="text-lg font-bold text-gray-700">
+          Total Pay: {calculateTotalPay().toFixed(2)}
+        </h4>
+      </div>
+
+      {/* Submit Button */}
       <button
         type="submit"
-        className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+        className="bg-gradient-to-r from-blue-500 to-blue-600 text-white px-4 py-2 rounded-lg hover:opacity-90"
       >
-        Save Payment Information
+        Save Information
       </button>
     </form>
   );
