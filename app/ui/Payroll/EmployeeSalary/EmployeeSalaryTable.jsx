2f5d57983c886775574004195344fb449017e66a
Hasibur14 <hasib141415@gmail.com>
2024-11-28 12:58:23 +0600

    [ADD] employee salary button added

--- app/ui/Payroll/EmployeeSalary/EmployeeSalaryTable.jsx
+++ app/ui/Payroll/EmployeeSalary/EmployeeSalaryTable.jsx
@@ -1,5 +1,8 @@
 "use client";
 import React, { useState } from "react";
+import { SiApacheopenoffice } from "react-icons/si";
+import { LiaFileDownloadSolid } from "react-icons/lia";
+import { ImSpinner9 } from "react-icons/im";
 
 const EmployeeSalaryTable = ({ employees }) => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -35,51 +38,68 @@ const EmployeeSalaryTable = ({ employees }) => {
     <div className="min-h-screen bg-gray-100 p-6">
       <h1 className="text-2xl font-bold mb-4">All Employees Salary</h1>
 
-      {/* Filter Section */}
-      <div className="bg-white p-6 rounded-lg shadow mb-8 grid grid-cols-1 md:grid-cols-3 gap-6">
-        {/* Search Bar */}
-        <div>
-          <label className="text-sm font-medium text-gray-600">Search</label>
-          <input
-            type="text"
-            placeholder="Search by any field"
-            className="mt-2 block w-full border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200"
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-        </div>
+      <div className="lg:flex justify-between">
+        {/* Filter Section */}
+        <div className="bg-white p-6 rounded-lg shadow mb-8 grid grid-cols-1 md:grid-cols-3 gap-6">
+          {/* Search Bar */}
+          <div>
+            <label className="text-sm font-medium text-gray-600">Search</label>
+            <input
+              type="text"
+              placeholder="Search by any field"
+              className="mt-2 block w-full border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200"
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </div>
 
-        {/* Filter by Department */}
-        <div>
-          <label className="text-sm font-medium text-gray-600">
-            Filter by Department
-          </label>
-          <select
-            value={filterDepartment}
-            onChange={(e) => setFilterDepartment(e.target.value)}
-            className="mt-2 block w-full border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200"
-          >
-            <option value="">All Departments</option>
-            <option value="HR">HR</option>
-            <option value="IT">IT</option>
-            <option value="Finance">Finance</option>
-          </select>
-        </div>
+          {/* Filter by Department */}
+          <div>
+            <label className="text-sm font-medium text-gray-600">
+              Filter by Department
+            </label>
+            <select
+              value={filterDepartment}
+              onChange={(e) => setFilterDepartment(e.target.value)}
+              className="mt-2 block w-full border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200"
+            >
+              <option value="">All Departments</option>
+              <option value="HR">HR</option>
+              <option value="IT">IT</option>
+              <option value="Finance">Finance</option>
+            </select>
+          </div>
 
-        {/* Rows per Page */}
-        <div>
-          <label className="text-sm font-medium text-gray-600">
-            Rows per Page
-          </label>
-          <select
-            value={rowsPerPage}
-            onChange={handleRowsPerPageChange}
-            className="mt-2 block w-full border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200"
-          >
-            <option value="10">10</option>
-            <option value="20">20</option>
-            <option value="50">50</option>
-            <option value="100">100</option>
-          </select>
+          {/* Rows per Page */}
+          <div>
+            <label className="text-sm font-medium text-gray-600">
+              Rows per Page
+            </label>
+            <select
+              value={rowsPerPage}
+              onChange={handleRowsPerPageChange}
+              className="mt-2 block w-full border px-4 py-2 rounded-lg shadow-sm focus:ring focus:ring-blue-200"
+            >
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="50">50</option>
+              <option value="100">100</option>
+            </select>
+          </div>
+        </div>
+        {/* download button */}
+        <div className="bg-white p-6 rounded-lg space-y-2 ">
+          <button type="button" class=" btn flex gap-3 text-white w-44  py-2 rounded-2xl bg-gradient-to-r from-orange-600 to-blue-500 hover:from-pink-500 hover:to-orange-500 ...">
+            <SiApacheopenoffice className="text-xl" />
+            WPS
+          </button>
+          <button type="button" class=" btn flex gap-3 text-white w-44  py-2 rounded-2xl bg-gradient-to-r from-teal-400 to-blue-500 hover:from-pink-500 hover:to-orange-500 ...">
+            <LiaFileDownloadSolid className="text-xl" />
+            Download
+          </button>
+          <button type="button" class=" btn flex gap-3 text-white w-44  py-2 rounded-2xl bg-gradient-to-r from-teal-400 to-blue-500 hover:from-pink-500 hover:to-orange-500 ...">
+            <ImSpinner9 className="text-xl" />
+            Refresh
+          </button>
         </div>
       </div>
 
@@ -153,11 +173,10 @@ const EmployeeSalaryTable = ({ employees }) => {
           <button
             onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
             disabled={currentPage === 1}
-            className={`px-4 py-2 rounded-lg ${
-              currentPage === 1
-                ? "bg-gray-200 text-gray-400 cursor-not-allowed"
-                : "bg-blue-500 text-white hover:bg-blue-600"
-            }`}
+            className={`px-4 py-2 rounded-lg ${currentPage === 1
+              ? "bg-gray-200 text-gray-400 cursor-not-allowed"
+              : "bg-blue-500 text-white hover:bg-blue-600"
+              }`}
           >
             Previous
           </button>
@@ -166,11 +185,10 @@ const EmployeeSalaryTable = ({ employees }) => {
               <button
                 key={pageIndex}
                 onClick={() => setCurrentPage(pageIndex + 1)}
-                className={`px-4 py-2 rounded-lg ${
-                  currentPage === pageIndex + 1
-                    ? "bg-blue-500 text-white"
-                    : "bg-gray-200 text-gray-700 hover:bg-gray-300"
-                }`}
+                className={`px-4 py-2 rounded-lg ${currentPage === pageIndex + 1
+                  ? "bg-blue-500 text-white"
+                  : "bg-gray-200 text-gray-700 hover:bg-gray-300"
+                  }`}
               >
                 {pageIndex + 1}
               </button>
@@ -181,11 +199,10 @@ const EmployeeSalaryTable = ({ employees }) => {
               setCurrentPage((prev) => Math.min(prev + 1, totalPages))
             }
             disabled={currentPage === totalPages}
-            className={`px-4 py-2 rounded-lg ${
-              currentPage === totalPages
-                ? "bg-gray-200 text-gray-400 cursor-not-allowed"
-                : "bg-blue-500 text-white hover:bg-blue-600"
-            }`}
+            className={`px-4 py-2 rounded-lg ${currentPage === totalPages
+              ? "bg-gray-200 text-gray-400 cursor-not-allowed"
+              : "bg-blue-500 text-white hover:bg-blue-600"
+              }`}
           >
             Next
           </button>
