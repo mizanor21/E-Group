0f4fec9a567de700f277674eafe5a24790f47b94
Hasibur14 <hasib141415@gmail.com>
2024-12-10 12:55:34 +0600

    [ADD] handle input field in hooks form

--- app/ui/Payroll/EmployeeSalary/CreateSalary/CreateSalary.jsx
+++ app/ui/Payroll/EmployeeSalary/CreateSalary/CreateSalary.jsx
@@ -1,11 +1,19 @@
+"use client"
 import React from "react";
+import { useForm } from "react-hook-form";
 import { CiEdit } from "react-icons/ci";
 
 const CreateSalary = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
     const inputStyle =
         "border rounded-md p-3 px-5 text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full";
     const btnStyle =
         "bg-sky-600 text-white px-10 py-3 rounded-md text-center hover:bg-white hover:border hover:border-sky-600 hover:text-sky-600 transition-all duration-500 mt-6";
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
 
     return (
         <div className="bg-white p-10 space-y-10">
@@ -30,24 +38,27 @@ const CreateSalary = () => {
             {/* Working Days Section */}
             <div className="mb-6">
                 <h4 className="text-lg font-semibold">Working Days</h4>
-                <div className="flex items-center gap-4  border rounded-xl p-5">
+                <form onSubmit={handleSubmit(onSubmit)} className="flex items-center gap-4  border rounded-xl p-5">
                     <input
+                        {...register("workingDays", { required: "This field is required" })}
                         type="number"
                         className={inputStyle}
                         placeholder="Enter days"
                     />
+                    {errors.workingDays && <p className="text-red-500 text-sm">{errors.workingDays.message}</p>}
                     <button className="bg-sky-600 text-white px-10 py-3 rounded-md text-center hover:bg-white hover:border hover:border-sky-600 hover:text-sky-600 transition-all duration-500">Process</button>
-                </div>
+                </form>
             </div>
 
             {/* Over Time Section */}
             <div>
                 <h4 className="text-lg font-semibold">Over Time</h4>
                 <div className="flex justify-between border rounded-xl border-gray-400 p-5">
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-4">
+                    <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-4">
                         <div>
                             <label className="block mb-2">Normal Day Over Time</label>
                             <input
+                                {...register("normalOverTime", { required: "This field is required" })}
                                 type="number"
                                 className={inputStyle}
                                 placeholder="Enter Hours"
@@ -56,6 +67,7 @@ const CreateSalary = () => {
                         <div>
                             <label className="block mb-2">Holiday Over Time</label>
                             <input
+                                {...register("holidayOverTime", { required: "This field is required" })}
                                 type="number"
                                 className={inputStyle}
                                 placeholder="Enter Hours"
@@ -64,6 +76,7 @@ const CreateSalary = () => {
                         <div>
                             <label className="block mb-2">Total Normal Over Time Earning</label>
                             <input
+                                {...register("normalOverTimeEarning")}
                                 type="text"
                                 className={inputStyle}
                                 placeholder="Leave It Empty It will Auto Generate"
@@ -73,6 +86,7 @@ const CreateSalary = () => {
                         <div>
                             <label className="block mb-2">Total Holiday Over Time Earning</label>
                             <input
+                                {...register("holidayOverTimeEarning")}
                                 type="text"
                                 className={inputStyle}
                                 placeholder="Leave It Empty It will Auto Generate"
@@ -80,7 +94,7 @@ const CreateSalary = () => {
                             />
                         </div>
                         <button className={btnStyle}>Process</button>
-                    </div>
+                    </form>
                     <div className="mt-6 border border-sky-600 rounded-lg p-6">
                         <h4 className="text-lg font-semibold text-center">Total Allowance Earning</h4>
                         <div className="text-center text-gray-500 mt-4">
@@ -95,17 +109,18 @@ const CreateSalary = () => {
                 <div>
                     <h3 className="text-lg font-semibold">Allowances</h3>
                     <div className="border rounded-lg p-5 shadow-sm">
-                        <div className="grid grid-cols-2 gap-4 mt-4">
-                            <input type="text" placeholder="Allowances" className={inputStyle} />
-                            <input type="text" placeholder="Special Allowances" className={inputStyle} />
-                            <input type="text" placeholder="Accommodation" className={inputStyle} />
-                            <input type="text" placeholder="Food Allowance" className={inputStyle} />
-                            <input type="text" placeholder="Telephone Allowance" className={inputStyle} />
-                            <input type="text" placeholder="Transport Allowance" className={inputStyle} />
-                        </div>
+                        <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-2 gap-4 mt-4">
+                            <input {...register("allowances")} type="text" placeholder="Allowances" className={inputStyle} />
+                            <input {...register("specialAllowances")} type="text" placeholder="Special Allowances" className={inputStyle} />
+                            <input {...register("accommodation")} type="text" placeholder="Accommodation" className={inputStyle} />
+                            <input {...register("foodAllowance")} type="text" placeholder="Food Allowance" className={inputStyle} />
+                            <input {...register("telephoneAllowance")} type="text" placeholder="Telephone Allowance" className={inputStyle} />
+                            <input {...register("transportAllowance")} type="text" placeholder="Transport Allowance" className={inputStyle} />
+                        </form>
                         <button className={btnStyle}>Process</button>
                         <h4 className="font-bold text-center mt-4">Allowance Earning</h4>
                         <input
+                            {...register("allowanceEarning")}
                             type="text"
                             placeholder="Leave It Empty It will Auto Generate"
                             className={inputStyle}
@@ -118,17 +133,17 @@ const CreateSalary = () => {
                 <div>
                     <h3 className="text-lg font-semibold">Deductions</h3>
                     <div className="border rounded-xl border-rose-500 p-5 shadow-sm">
-                        <div className="grid grid-cols-2 gap-4 mt-4">
-                            <input type="text" placeholder="Number of Leave" className={inputStyle} />
-                            <input type="text" placeholder="Ded Leave" className={inputStyle} />
-                            <input type="text" placeholder="Ded-Fines" className={inputStyle} />
-                            <input type="text" placeholder="Ded-Doc" className={inputStyle} />
-                            <input type="text" placeholder="Ded-Others" className={inputStyle} />
-                            <button className={btnStyle}>Process</button>
-                        </div>
-
+                        <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-2 gap-4 mt-4">
+                            <input {...register("numberOfLeave")} type="text" placeholder="Number of Leave" className={inputStyle} />
+                            <input {...register("dedLeave")} type="text" placeholder="Ded Leave" className={inputStyle} />
+                            <input {...register("dedFines")} type="text" placeholder="Ded-Fines" className={inputStyle} />
+                            <input {...register("dedDoc")} type="text" placeholder="Ded-Doc" className={inputStyle} />
+                            <input {...register("dedOthers")} type="text" placeholder="Ded-Others" className={inputStyle} />
+                        </form>
+                        <button className={btnStyle}>Process</button>
                         <h4 className="font-bold text-red-500 text-center mt-4">Deduction</h4>
                         <input
+                            {...register("deduction")}
                             type="text"
                             placeholder="Leave It Empty It will Auto Generate"
                             className={`${inputStyle} border-red-500`}
@@ -143,14 +158,15 @@ const CreateSalary = () => {
                 <h3 className="text-lg font-semibold">Others</h3>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
                     <div className="border rounded-lg p-5 shadow-sm">
-                        <div className="grid grid-cols-2 gap-4 mt-4">
-                            <input type="text" placeholder="Adv Recovery" className={inputStyle} />
-                            <input type="text" placeholder="Arrear Payments" className={inputStyle} />
-                            <input type="text" placeholder="Current Balance" className={inputStyle} />
-                        </div>
+                        <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-2 gap-4 mt-4">
+                            <input {...register("advRecovery")} type="text" placeholder="Adv Recovery" className={inputStyle} />
+                            <input {...register("arrearPayments")} type="text" placeholder="Arrear Payments" className={inputStyle} />
+                            <input {...register("currentBalance")} type="text" placeholder="Current Balance" className={inputStyle} />
+                        </form>
                         <button className={btnStyle}>Process</button>
-                        <h4 className="font-bold text-center mt-4">Other Earning</h4>
+                        <h4 className="font-bold text-center text-2xl mt-4">Other Earning</h4>
                         <input
+                            {...register("otherEarning")}
                             type="text"
                             placeholder="Leave It Empty It will Auto Generate"
                             className={inputStyle}
@@ -161,7 +177,9 @@ const CreateSalary = () => {
                     {/* Net Salary Section */}
                     <div className="border-dashed border border-gray-400 rounded-lg p-5 shadow-sm">
                         <h3 className="text-2xl text-center font-semibold">Net Salary</h3>
+                        <label className="block mb-2">Net Salary</label>
                         <input
+                            {...register("netSalary")}
                             type="text"
                             placeholder="Enter The Amount"
                             className={inputStyle}
