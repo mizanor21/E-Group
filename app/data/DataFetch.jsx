7996c888dbcba29fed63aca3bd62a19c47627fce
mizanor21 <mizanorrahman398@gmail.com>
2025-02-05 20:37:37 +0600

    some polishing

--- app/data/DataFetch.jsx
+++ app/data/DataFetch.jsx
@@ -3,38 +3,37 @@ import useSWR from "swr";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
 export const useCompanyData = () => {
-  const { data, error } = useSWR("http://localhost:3000/api/company", fetcher);
+  const { data, error } = useSWR(`${API_URL}/api/company`, fetcher);
   return { data, error, isLoading: !data && !error };
 };
 
 export const useProjectData = () => {
-  const { data, error } = useSWR("http://localhost:3000/api/projects", fetcher);
+  const { data, error } = useSWR(`${API_URL}/api/projects`, fetcher);
   return { data, error, isLoading: !data && !error };
 };
 
 export const useGroupData = () => {
-  const { data, error } = useSWR("http://localhost:3000/api/group", fetcher);
+  const { data, error } = useSWR(`${API_URL}/api/group`, fetcher);
   return { data, error, isLoading: !data && !error };
 };
 
 export const useEmployeeData = () => {
-  const { data, error } = useSWR(
-    "http://localhost:3000/api/employees",
-    fetcher
-  );
+  const { data, error } = useSWR(`${API_URL}/api/employees`, fetcher);
   return { data, error, isLoading: !data && !error };
 };
 
 export const useEmployeeDetailsData = ({ params }) => {
   const { data, error } = useSWR(
-    params?.id ? `http://localhost:3000/api/employees/${params.id}` : null,
+    params?.id ? `${API_URL}/api/employees/${params.id}` : null,
     fetcher
   );
   return { data, error, isLoading: !data && !error };
 };
 
 export const useSalaryData = () => {
-  const { data, error } = useSWR(`http://localhost:3000/api/salary`, fetcher);
+  const { data, error } = useSWR(`${API_URL}/api/salary`, fetcher);
   return { data, error, isLoading: !data && !error };
 };
