978023b59353c7155182dbf0cd3d283e4f9b76a9
mizanor21 <mizanorrahman398@gmail.com>
2025-02-18 16:22:03 +0600

    some polishing

--- app/dashboard/(Accounts)/accounts/page.jsx
+++ app/dashboard/(Accounts)/accounts/page.jsx
@@ -1,5 +1,9 @@
+import AccountsDashboard from "@/app/ui/Accounts/AccountsDashboard"
+
 const Page = ({}) => {
-  return <div>this is accounts module!</div>
+  return <div>
+    <AccountsDashboard />
+  </div>
 }
 
 export default Page
