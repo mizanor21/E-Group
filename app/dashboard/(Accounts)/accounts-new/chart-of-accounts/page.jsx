d67ce2ea4b4b99cfae8bf083c76ff3837a042356
mizanor21 <mizanorrahman398@gmail.com>
2025-05-05 11:36:25 +0600

    chart of accounts part added

--- app/dashboard/(Accounts)/accounts-new/chart-of-accounts/page.jsx
+++ app/dashboard/(Accounts)/accounts-new/chart-of-accounts/page.jsx
@@ -1,8 +1,9 @@
+import FinancialDashboard from "@/app/ui/Accounts/ChartOfAccounts/FinancialDashboard";
+
 export default function AccountsNew() {
     return (
         <div>
-            <h1>Accounts New Page</h1>
-            <p>Welcome to the Accounts New Page!</p>
+            <FinancialDashboard />
         </div>
     );
 }
