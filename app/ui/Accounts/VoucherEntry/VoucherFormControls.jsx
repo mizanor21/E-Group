6f95cc3a2b6ba50f250d0d857d0d5055081e90fb
mizanor21 <mizanorrahman398@gmail.com>
2025-05-10 10:49:50 +0600

    some polishing

--- app/ui/Accounts/VoucherEntry/VoucherFormControls.jsx
+++ app/ui/Accounts/VoucherEntry/VoucherFormControls.jsx
@@ -101,11 +101,8 @@ export const VoucherFormHeader = ({ control, register, watch }) => {
                         <ChevronDown className="absolute right-2 top-2.5 h-4 w-4 text-gray-500" />
                     </div>
                 </div>
-            </div>
 
-            {/* Form Header - Row 2 */}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-6">
-            <DateSelector
+                <DateSelector
                 control={control}
                 name="date"
                 label="Select Date"
